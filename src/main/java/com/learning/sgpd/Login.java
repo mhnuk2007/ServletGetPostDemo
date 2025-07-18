c0e668c56bb8fc4cbf31424455bf27081fb71779
Mohan Lal <mhnuk2007@gmail.com>
2025-07-18 14:57:11 +0500

    Updated Login servlet and index.html to display method used and login status

--- src/main/java/com/learning/sgpd/Login.java
+++ src/main/java/com/learning/sgpd/Login.java
@@ -6,22 +6,47 @@ import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.io.PrintWriter;
 
 
 @WebServlet("/mylogin")
 public class Login extends HttpServlet {
 		
-	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		handlelogin(request, response, "GET");
+		
+	}
+	
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		handlelogin(request, response, "POST");
+		
+	}
+	
+	
+	private void handlelogin(HttpServletRequest request, HttpServletResponse response, String method) throws IOException {
 		String myemail = request.getParameter("email1");
 		String mypass = request.getParameter("pass1");
 		
+		response.setContentType("text/html");
+		PrintWriter out = response.getWriter();
+		
+		out.printf("<h2>Login Attempt using %s method</h2>", method );
+		out.printf("<h3>Email: %s</h3>", myemail);
+		out.printf("<h3>Password: %s</h3>", mypass);
+		
 		if(myemail.equals("honey@email.com") && mypass.equals("1234")) {
-			response.getWriter().println("Login Successful");
+			out.println("<p style='color:green;'>Login Successful</p>");
 			
 		} else {
-			response.getWriter().println("Login failed");
+			out.println("<p style='color:red;'>Login Failed</p>");
+
 		}
 		
+		// for get method, we can also print the query string
+		if(method.equals("GET")) {
+			String queryString = request.getQueryString();
+			out.printf("<h3>Query String: %s</h3>", queryString);
+		}
 		
 	}
 
